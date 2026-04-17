cbf4cf20aeda6c582ca34df28a3ba323d3eaa3d9
michaelrauh <michaelrauh@ymail.com>
2026-04-17 17:16:27 -0400

    d3p1

--- three.hs
+++ three.hs
@@ -1,10 +1,46 @@
-processOne :: String
-processOne = "hello"
+{-# LANGUAGE TupleSections #-}
+import Data.Char (digitToInt)
+import Data.List (tails)
+newtype Battery = Battery Int
+newtype Bank = Bank [Battery]
+
+unBank :: Bank -> [Battery]
+unBank (Bank bs) = bs
+
+
+-- example input
+-- 987654321111111
+-- 811111111111119
+-- 234234234234278
+-- 818181911112111
+
+parse :: String -> Bank
+parse = Bank . map (Battery . digitToInt)
+
+parseAll :: String -> [Bank]
+parseAll = map parse . lines
+
+processOne :: [Bank] -> Int
+processOne = sum . map findMaxJoltage
+
+pairs :: [a] -> [(a, a)]
+pairs =
+  concatMap (\(x:xs) -> map (x,) xs)
+  . filter (not . null)
+  . tails
+
+findMaxJoltage :: Bank -> Int
+findMaxJoltage = maximum . map pairToJoltage . pairs . unBank
+
+pairToJoltage :: (Battery, Battery) -> Int
+pairToJoltage (Battery a, Battery b) = 10 * a + b
 
 process :: String -> IO ()
 process input = do
-  putStrLn $ "Part 1: " ++ show processOne
+    let banks = parseAll input
+    putStrLn $ "Part 1: " ++ show (processOne banks)
 
+main :: IO ()
 main = do
   s <- readFile "inputs/3.txt"
   process s
